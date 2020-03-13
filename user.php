95b41fe74048e9f2806300978b4a571d623b2d9a
Shiva <shivaxsin@gmail.com>
2020-03-13 09:07:57 +0800

    feat: user name

---
+++ user.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action = "board.php" method="post">
+你的名字:<input type ="text" name="user" />
+    <input type="submit" value="開始留言" onclick="board.php">
+</form>
+
+</body>
+</html>
