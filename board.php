d8a90677ce5b89f9e5f6ea4e596efe1ff3b806d8
Shiva <shivaxsin@gmail.com>
2020-03-16 00:13:08 +0800

    fix: 初階完成-畫面大改，移除td、tr，改用 div 處理

--- board.php
+++ board.php
@@ -36,11 +36,11 @@ $callboards = $conn->query($boards_desc);
 
 <table border = "1" align = "center">
 
-    <tr>
-        <th>番號</th>
-        <th>留言</th>
-        <th>回覆內容</th>
-    </tr>
+<!--    <tr>-->
+<!--        <th>番號</th>-->
+<!--        <th>留言</th>-->
+<!--        <th>回覆內容</th>-->
+<!--    </tr>-->
 
 <!--<form action = "look.php" method="GET">-->
 <?php
@@ -48,25 +48,26 @@ $callboards = $conn->query($boards_desc);
 //echo "<input type='text' value='hi'>";
 
 foreach ($callboards as $end){
-    echo  "<tr>";
 //    echo  "<td><input type='text' value='{$end['id']}'></td>";
-    echo  "<td>
-            <div align='center'>{$end['id']}</div>
-            </td>";
-    echo  "<td>
-            <div><font size='2' color='#8b4513'>留言者：</font>{$end['author']}</div>
-            <div><font size='2' color='#d2691e'>留言內容：</font>
-            <div style='border:3px #98685cd8 ridge'>{$end['content']}</div>
-            <div><font size='1' color='#87ceeb'>留言時間：{$end['create_time']}</font></div>
-            <br>
+
+//    echo  "<td>";
+        echo"<div style='border:5px #408080 groove'>";
+        echo"
+            <div style='border:3px 	#FF7575 ridge'>
+            <font size='2' color='#8b4513'>番號：{$end['id']}</font><br>
+            <font size='2' color='#8b4513'>留言者：</font>{$end['author']}<br>
+            <font size='1' color='#9D9D9D'>留言時間：{$end['create_time']}</font><br>
+            <font size='2' color='#d2691e'>留言內容</font>
             
+            <div>{$end['content']}<br></div>
             <form action = 'msg.php' method= 'POST'>
             <input type = 'hidden' name='board_id' value='{$end['id']}'>
             <input type = 'hidden' name='msg_user' value=$user >
-            <div><font size='2' color='#006400'>評論留言：</font><input type='text' name='msg'></div>
-            <div><input type='submit' name='submit' value='進行評論'></div>
+            <font size='2' color='#006400'>評論留言：</font><input type='text' name='msg'><br>
+            <input type='submit' name='submit' value='進行評論'></div>
             </form>
-            </td>>";
+            <br>";
+//            </td>";
 
     $msgs_desc = "SELECT * FROM msgs ORDER  BY id DESC";
     $callmsgs = $conn->query($msgs_desc);
@@ -77,21 +78,24 @@ foreach ($callboards as $end){
             if($end['id'] != $msg_end['boards_id']){
                 continue;
             }
-                echo "<td>
-                    <div><font size='1' color='#8b4513'>評論號：</font>{$msg_end['id']}</div>
-                    <div><font size='2' color='#f08080'>評論者：</font>{$msg_end['msg_user']}</div>
-                    <div><font size='2' color='#f08080'>評論內容：</font></div>
-                    <div style='border:3px #98bb5cd8 ridge'><font size='1'>{$msg_end['msg']}</div>
-                    <div><font size='1' color='#a9a9a9'>評論時間：{$msg_end['create_time']}</font></div>
-                    <br>
+//                echo "<td>
+                echo"
+                  
+                    <div style='border:3px 	#FFBB77 ridge'	><font size='1' color='#8b4513'>評論番號：</font>{$msg_end['id']}<br>
+                    <font size='2' color='#f08080'>評論者：</font>{$msg_end['msg_user']}<br>
+                    <font size='1' color='#a9a9a9'>評論時間：{$msg_end['create_time']}</font><br>
+                    <font size='2' color='#f08080'>評論內容</font><br>
+                    <font size='1' color='black'>{$msg_end['msg']}</font>
+                    
                     <form action = 'remsg.php' method = 'POST'>
                     <input type= 'hidden' name= 'board_id' value='{$msg_end['boards_id']}'>
                     <input type= 'hidden' name= 'msg_id' value ='{$msg_end['id']}'>
                     <input type= 'hidden' name= 'remsg_user' value = $user>
-                    <div>回覆評論：<input type= 'text' name= 'remsg'></div>
+                    <font size='1' color='teal'>回覆評論：<input type= 'text' name= 'remsg'><br>
                     <div><input type='submit' name='submit' value='回覆'></div>
                     </form>
-                    ";
+                    </div>
+                    <br>";
 
             $remsgs_desc = "SELECT * FROM remsgs ORDER BY id DESC";
             $callremsgs = $conn->query($remsgs_desc);
@@ -110,22 +114,21 @@ foreach ($callboards as $end){
 //                        <div><font size='2' color='#f08080'>回覆者：</font>{$remsg_end['remsg_user']}</div>
 //
 //                    </td>";
+
+
                     echo "
-                         <div style='border:3px #5CADAD ridge'><font size='1'>
-                        <div><font size='1' color='#8b4513'>留言號：</font>{$remsg_end['boards_id']}</div>
-                        <div><font size='1' color='#8b4513'>評論號：</font>{$remsg_end['msg_id']}</div>
-                        <div><font size='2' color='#f08080'>回覆者：</font>{$remsg_end['remsg_user']}</div>
-                        <div><font size='2' color='#f08000'>回覆內容：</font>{$remsg_end['remsg']}</div>
-                       </div>"
+                         <div'><font size='1'>
+                        <div><font size='1' color='#8b4513'>
+                        <font size='1' color='#f08080'>回覆者：</font>{$remsg_end['remsg_user']}<br>
+                        <font size='1' color='#f08000'>回覆內容</font><br>
+                        {$remsg_end['remsg']}
+                       </div><br>"
                     ;
-
-
-
             }
-            echo "</td>";
+            echo "</div>";
         }
 
-    echo  "</tr>";
+
 
 }
 
