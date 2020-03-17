800b0a40f55bc586ee92e0809b97754e14f23960
Shiva <shivaxsin@gmail.com>
2020-03-18 00:53:50 +0800

    feat: 功能-新增留言

--- board/ormnewboard.php
+++ board/ormnewboard.php
@@ -1,1 +1,32 @@
-<?php
+<?php session_start();
+require "bootstrap.php";
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Carbon\Carbon;
+
+
+
+if (isset($_POST["author"])) {
+
+    $author = $_POST["author"];
+    $content = $_POST["content"];
+    $create_time = Carbon::now();
+
+    Capsule::table('boards')->insert
+    ([
+        'author' => $author,
+        'content' => $content,
+        'create_time' => $create_time,
+        ]);
+
+
+
+    echo "新增留言成功";
+
+
+
+    $url = "ormboard.php";
+    echo "<script language='javascript' type='text/javascript'>";
+    echo "window.location.href='$url'";
+    echo "</script>";
+
+}
