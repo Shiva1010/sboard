bb7a5b506b0d802a0e5eb14a6ed2d52f1ce5c34c
Shiva <shivaxsin@gmail.com>
2020-03-18 09:03:37 +0800

    feat: 畫面+功能 依新舊排列 查看留言

--- board/ormboard.php
+++ board/ormboard.php
@@ -1,5 +1,6 @@
 <?php session_start();
 require "bootstrap.php";
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 if (isset($_POST['user'])) {
 
@@ -10,9 +11,9 @@ $user = $_SESSION['user'];
 
 ?>
 
-<html>
-<meta charset="UTF-8"/>
-<title>Shiva の　留言板</title>
+    <html>
+    <meta charset="UTF-8"/>
+    <title>Shiva の　留言板</title>
 <body>
 <div style="text-align: center">
     <font size='6' color='#02C88b'>Shiva の 留言板</font><br><br>
@@ -32,3 +33,27 @@ $user = $_SESSION['user'];
         <!-- <input type="hidden" name=$_POST['id']>-->
     </form>
 </div>
+
+<?php
+
+
+    $boards_desc = Capsule::table('boards')
+        ->select()
+        ->orderBy('id','desc')
+        ->get();
+
+
+//    $callboards = $conn->query($boards_desc);
+
+    foreach ($boards_desc as $end) {
+        echo "
+                <div style='border:3px 	#FF7575 ridge'>
+                <font size='2' color='#8b4513'>番號：{$end->id}</font><br>
+                <font size='2' color='#8b4513'>留言者：</font>{$end->author}<br>
+                <font size='1' color='#9D9D9D'>留言時間：{$end->create_time}</font><br>
+                <br>
+                <font size='2' color='#8b4513'>留言內容</font><br>
+                
+                {$end->content}<br>";
+
+    }
